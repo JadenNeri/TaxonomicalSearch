8d4ca5bba76040a195495207fdd82210ddfac8bc
Laurence <laurenceschrade@gmail.com>
2025-11-03 12:01:36 -0500

    DFS and BFS basic structure; remember to update CMake on your ends

---
+++ src/traversals.cpp
@@ -0,0 +1,5 @@
+//
+// Created by nitro on 11/2/2025.
+//
+
+#include "traversals.h"
---
+++ src/traversals.h
@@ -0,0 +1,16 @@
+//
+// Created by nitro on 11/2/2025.
+//
+
+#ifndef TRAVERSALS_H
+#define TRAVERSALS_H
+
+
+
+class traversals {
+
+};
+
+
+
+#endif //TRAVERSALS_H
